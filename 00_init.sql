5cc55a2029589ce8733d614408202f8ceaa75f98
Denis <dpashnev175@gmail.com>
2025-08-01 15:37:33 +0200

    Updated docker-compose.yml. Implemented master-slave replication architecture.

--- 00_init.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator';
-SELECT pg_create_physical_replication_slot('replication_slot');
--- 01_hba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cat >> "$PGDATA/pg_hba.conf" <<EOF
-host replication all 0.0.0.0/0 md5
-EOF
