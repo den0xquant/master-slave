68e3f1f084cc2bc3506514e019ae5ea5f71c315f
Denis <dpashnev175@gmail.com>
2025-08-04 03:04:25 +0200

    added healthcheck

--- project/app.py
+++ project/app.py
@@ -3,4 +3,11 @@ from project.routes.heroes import heroes_router
 
 
 app = FastAPI(title="Master Slave architecture")
+
+
+@app.get("/healthy")
+def healthcheck() -> bool:
+    return True
+
+
 app.include_router(heroes_router)
